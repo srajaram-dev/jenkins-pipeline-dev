ea5d42aa9c84830466f38238dc4379913865bfe9
senthilkumarrajaram <senthilkumar.rajaram@esure.com>
2020-10-22 12:04:36 +0100

    Initial version

---
+++ vars/pipelineLibrary.groovy
@@ -0,0 +1,45 @@
+def call(Map pipelineParams) 
+{
+  pipeline
+  {	
+    agent any	
+      stages	
+      {		
+          stage('environment')		
+          {			
+              steps			
+              {				
+                  echo "ENV_NAME :::::: ${pipelineParams.ENV_NAME}"	
+              }		
+           }		
+           stage('mavenBuild')
+           {	
+              steps			
+              {				
+                  echo "Maven Build :::::: ${pipelineParams.ENV_NAME}"	
+               }		
+            }	
+           stage('codeScan')
+           {	
+              steps			
+              {				
+                  echo "Scan code :::::: ${pipelineParams.ENV_NAME}"	
+               }		
+            }	
+            stage('dockerBuild')
+           {	
+              steps			
+              {				
+                  echo "Docker Build :::::: ${pipelineParams.ENV_NAME}"	
+               }		
+            }	
+            stage('dockerDeploy')
+           {	
+              steps			
+              {				
+                  echo "Docker Deploy :::::: ${pipelineParams.ENV_NAME}"	
+               }		
+            }	
+         }
+      }
+}
