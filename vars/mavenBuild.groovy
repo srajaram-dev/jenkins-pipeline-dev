74133a49a728fa11e5bd1d44060c6df5e16528ed
srajaram-dev <senthilrajaram.uk@gmail.com>
2020-10-23 07:26:34 +0100

    Update

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,4 +1,7 @@
-echo ":::::::::::::: Executing mavenBuild.groovy :::::::::::::: "	
-def scriptContent = libraryResource "com/dev/scripts/mavenBuild.sh"
-writeFile file: "mavenBuild.sh", text: scriptContent
-sh 'mavenBuild.sh "mavenBuild.sh"'
+def call() 
+{
+	echo ":::::::::::::: Executing mavenBuild.groovy :::::::::::::: "	
+	def scriptContent = libraryResource "com/dev/scripts/mavenBuild.sh"
+	writeFile file: "mavenBuild.sh", text: scriptContent
+	sh 'mavenBuild.sh "mavenBuild.sh"'
+}
