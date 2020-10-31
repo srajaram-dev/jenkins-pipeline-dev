46f1fb9a3c9f22560b0f6a327aa3d04fc04be0dd
senthilkumarrajaram <senthilkumar.rajaram@esure.com>
2020-10-31 08:08:12 +0000

    Updated for maven settings

--- resources/com/dev/scripts/mavenBuild.sh
+++ resources/com/dev/scripts/mavenBuild.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 echo ":::::::::: Executing maven build ::::::::: ${MVN_REPO_USER}"
+
+# Copy over our settings.xml so the build can resolve dependencies
+cp mvn.settings.xml ${HOME}/.m2/settings.xml && ls -la ${HOME}/.m2/settings.xml && curl ifconfig.co
+
+
 mvn clean install
--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,6 +1,9 @@
 def call() 
 {
 	echo ":::::::::::::: Executing mavenBuild.groovy - START :::::::::::::: "
+	
+	def mavenSettings = libraryResource "com/dev/configs/mvn.settings.xml"
+	writeFile file: "mvn.settings.xml", text: mavenSettings
 		
 	def scriptContent = libraryResource "com/dev/scripts/mavenBuild.sh"
 	writeFile file: "mavenBuild.sh", text: scriptContent
